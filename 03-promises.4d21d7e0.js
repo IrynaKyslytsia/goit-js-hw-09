dc511d2f6d4312f326c221479f7dffa519b04709
IrynaKyslytsia <114502274+IrynaKyslytsia@users.noreply.github.com>
2023-03-18 19:14:10 +0000

    Deploying to gh-pages from @ IrynaKyslytsia/goit-js-hw-09@0709e240901b186da04f60b5c53b226cb011f98e 🚀

---
+++ 03-promises.4d21d7e0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");function o(e,o){const t=Math.random()>.3;return new Promise(((n,i)=>{setTimeout((()=>{t?n({position:e,delay:o}):i({position:e,delay:o})}),o)}))}e.addEventListener("submit",(function(t){t.preventDefault();let n=Number(e.delay.value);for(let t=1;t<=e.amount.value;t+=1)o(t,n).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)})),n+=Number(e.step.value)}));
+//# sourceMappingURL=03-promises.4d21d7e0.js.map
